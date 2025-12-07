9dd914863394151a3de9ea0d7360860c889e22d9
cristhianvk-bit <cristinaparitito@gmail.com>
2025-12-06 22:54:11 -0400

    Subida de Clase Mascota, Nueva Cita WPF, Nueva Cita CS

---
+++ Mascota.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Michis_Veterinaria
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public string Raza { get; set; }
+        public int IdDueno { get; set; }  
+    }
+}
---
+++ NuevaCita.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Michis_Veterinaria
+{
+    /// <summary>
+    /// Lógica de interacción para NuevaCita.xaml
+    /// </summary>
+    public partial class NuevaCita : Window
+    {
+        private ClienteMascota ventanaPrincipal;
+        public NuevaCita(ClienteMascota ventana)
+        {
+            InitializeComponent();
+            this.ventanaPrincipal = ventana;
+        }
+        private void GuardarRe(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNombreCliente.Text) ||
+                string.IsNullOrWhiteSpace(txtTelefonoCliente.Text) ||
+                string.IsNullOrWhiteSpace(txtCorreoCliente.Text) ||
+                string.IsNullOrWhiteSpace(txtNombreMascota.Text) ||
+                string.IsNullOrWhiteSpace(txtTipoMascota.Text) ||
+                string.IsNullOrWhiteSpace(txtRazaMascota.Text))
+            {
+                MessageBox.Show("Todos los campos son obligatorios.");
+                return;
+            }
+
+            Cliente c = new Cliente()
+            {
+                Nombre = txtNombreCliente.Text,
+                Telefono = txtTelefonoCliente.Text,
+                Correo = txtCorreoCliente.Text
+            };
+
+            Mascota m = new Mascota()
+            {
+                Nombre = txtNombreMascota.Text,
+                Tipo = txtTipoMascota.Text,
+                Raza = txtRazaMascota.Text
+            };
+
+            ventanaPrincipal.AgregarRegistro(c, m);
+
+            MessageBox.Show("Registro añadido correctamente");
+            this.Close();
+        }
+
+        private void Cerrar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
